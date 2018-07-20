fae0c7a02a0ebfd948c8614d84e210951c2a3065
Jakub Pawlicki <jakub.pawlicki@outlook.com>
2018-07-20 19:35:29 +0200

    azure kubernetes service - az cli service principal problem unsolved

--- bash/kube_docker.sh
+++ bash/kube_docker.sh
@@ -39,22 +39,66 @@ function local-app-setup() {
   rm -rf azure-voting-app-redis
 }
 
-local-app-setup
+is_local_app_set_in_docker=`sudo docker images | awk 'BEGIN {count=0} $1 == "azure-vode-front" {count++} END {print count}'`
+if [ $is_local_app_set_in_docker -gt 0 ]; then
+  local-app-setup
+fi
+unset is_local_app_set_in_docker
+
 #create azure container registry
 #https://docs.microsoft.com/en-gb/azure/aks/tutorial-kubernetes-prepare-acr
 group_name=aResourceGroup
 location=eastus
-acrName=aAcr`uuidgen | cut -c1-8`
-az group create --name $group_name --location $location
-az acr create --resource-group $group_name --name $acrName --sku Basic
+#indempotent?
+if [ 0 -eq $(az group show --name $group_name -o tsv | wc -l) ]; then
+  az group create --name $group_name --location $location
+fi
+
+container_registry_count=`az acr list -o tsv | wc -l`
+
+if [ $container_registry_count -eq 0 ]; then
+  acrName=aAcr`uuidgen | cut -c1-8`
+  az acr create --resource-group $group_name --name $acrName --sku Basic
+  return $acrName
+else
+  acrName=$(az acr list | jq -r '.[].name' | head -1) 
+fi
+  
 sudo az acr login --name $acrName
 login_server_of_acr=`az acr list -g $group_name --query "[].{arcLoginServer:loginServer}" -o tsv`
-sudo docker tag azure-vote-front $login_server_of_acr/azure-vote-front:v1
-#one can put entire applications into the docker registry on azure. nice
-#would this work with gui applications?
-sudo docker push $login_server_of_acr/azure-vote-front:v1
-acr_registry=`az acr list -o table | awk 'END {print $1}'`
-acr_repo=`az acr repository list --name $acr_registry --output table` | awk 'END {print $1}'`
-az acr repository show-tags --name $acr_registry --repository $acr_repo --output table
+is_local_app_tagged_for_azure=$(sudo docker images | awk "BEGIN {count=0} \$1 == \"${login_server_of_acr}/azure-vote-front\",\$2 == \"v1\" {count++} END {print count}")
+if [ $is_local_app_tagged_for_azure -eq 0 ]; then
+  sudo docker tag azure-vote-front $login_server_of_acr/azure-vote-front:v1
+fi
 
+proper_registry_on_azure=`az acr repository list --name ${acrName} -o tsv | grep -F 'azure-vote-front' | wc -l`
+proper_container_version=`az acr repository show-tags --name ${acrName} --repository azure-vote-front -o tsv | grep -F 'v1' | wc -l`
+if [ $proper_registry_on_azure -eq 0 ] || [ $proper_container_version -eq 0 ]; then
+  #one can put entire applications into the docker registry on azure. nice
+  #would this work with gui applications?
+  sudo docker push $login_server_of_acr/azure-vote-front:v1
+fi
+
+#acr_registry=`az acr list -o table | awk 'END {print $1}'`
+#acr_repo=`az acr repository list --name $acr_registry --output table | awk 'END {print $1}'`
+#az acr repository show-tags --name $acr_registry --repository $acr_repo --output table
+
+cluster_name=aksclust`uuidgen | cut -c 1-8 | tr '[:upper:]' '[:lower:]'`
+
+rbac_service_principal=$(az ad sp create-for-rbac)
+echo $rbac_service_principal | jq
+app_id=$(echo $rbac_service_principal | jq -r '.appId')
+passw=$(echo $rbac_service_principal | jq -r '.password')
+acr=$(az acr show --name $acrName -g $group_name)
+acr_id=$(echo $acr | jq -r '.id')
+echo $acr | jq
+az role assignment create --assignee "$app_id" --role Reader --scope "$acr_id"
+
+az aks create --name $cluster_name -g $group_name --node-count 1 --generate-ssh-keys --service-principal $app_id --client-secret "$passw"
+
+#doesnt work. better to set up kubernetes on raw vms
+#error:
+#Deployment failed. Correlation ID: 54ad6991-d903-4d54-be4d-262d89e493b6. getting current state of the cluster returned an error:
+# compute.VirtualMachinesClient#List: Failure responding to request: StatusCode=401 -- Original Error: autorest/azure: Service returned an error. Status=401 Code="InvalidAuthenticationToken" Message="The received access token is not valid: at least one of the claims 'puid' or 'altsecid' or 'oid' should be present. If you are accessing as application please make sure service principal is properly created in the tenant."
+# o
 
