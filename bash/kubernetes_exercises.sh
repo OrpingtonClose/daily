271126cc30e0ec8c6e4201e27910699359e7e8f7
Jakub Pawlicki <jakub.pawlicki@outlook.com>
2018-08-19 11:51:26 +0200

    kubernetes exercises

--- bash/kubernetes_exercises.sh
+++ bash/kubernetes_exercises.sh
@@ -2,151 +2,173 @@ gcloud auth login
 PROJECT=$(gcloud projects list | awk 'FNR==2 {print $1}')
 gcloud config set project $PROJECT
 gcloud config set compute/zone europe-west1
-gcloud container clusters create kubia --num-nodes 2 --machine-type
+gcloud container clusters create kubia --num-nodes 2 --machine-type f1-micro
 gcloud container clusters get-credentials kubia
 
 cat <<EOF | kubectl create -f -
 apiVersion: v1
 kind: Pod
 metadata:
-  name: kubia-manual
+  name: test-pod
+  namespace: default
 spec:
   containers:
-  - image: luksa/kubia
-    name: kubia
+  - name: service
+    image: nginx
     ports:
-    - containerPort: 8080
-      protocol: TCP
+    - containerPort: 80
+    env:
+    - name: SOME_ENV_VAR
+      value: Hello World
 EOF
 
-kubectl get po kubia-manual -o yaml
-kubectl get po kubia-manual -o 
+ReplicaSet ensures that a specified number of pod replicas are running at any given time.
 
-#port-forwarding
-LOCAL_PORT=8888
-CONTAINER_PORT=8080
-kubectl port-forward kubia-manual $LOCAL_PORT:$CONTAINER_PORT &
-curl localhost:$LOCAL_PORT
-
-#logs from containers, which are collected from stdout and stderr
-#logs rotate 10 mb limit
-kubectl logs kubia-manual 
-kubectl logs kubia-manual -c kubia
-
-#labels
 cat <<EOF | kubectl create -f -
-apiVersion: v1
-kind: Pod
+apiVersion: apps/v1
+kind: ReplicaSet
 metadata:
-  name: kubia-label
-  labels:
-    environment: prod
-    something: not-bad
+  name: my-service
 spec:
-  containers:
-  - image: luksa/kubia
-    name: kubia
-    ports:
-    - containerPort: 8080
-      protocol: TCP
+  replicas: 3
+  selector:
+    matchLabels:
+      app: my-service
+  template:
+    metadata:
+      labels:
+        app: my-service
+    spec:
+      containers:
+      - name: service
+        image: nginx
 EOF
 
-#set pod with label. value is empty for the pod, so this will work
-kubectl label pods kubia-manual environment=dev
-
-#when overwriting value, put add a flag
-kubectl label pods kubia-manual environment=devel --overwrite
-
-#show pod info with labels
-kubectl get po -L environment,something
-
-#show pods with labels 
-kubectl get po -l environment=prod
-#NAME          READY     STATUS    RESTARTS   AGE       ENVIRONMENT   SOMETHING
-#kubia-label   1/1       Running   0          11m       prod          not-bad
-
-kubectl get pods -l something
-#NAME          READY     STATUS    RESTARTS   AGE       ENVIRONMENT   SOMETHING
-#kubia-label   1/1       Running   0          11m       prod          not-bad
-
-kubectl get pods -l '!something'
-#NAME           READY     STATUS    RESTARTS   AGE       SOMETHING   ENVIRONMENT
-#kubia-manual   1/1       Running   0          44m                   devel
-
-kubectl get pods -l 'environment in (not,devel)'
-#NAME           READY     STATUS    RESTARTS   AGE       ENVIRONMENT   SOMETHING
-#kubia-manual   1/1       Running   0          41m       devel         
-
-kubectl get pods -l 'environment notin (not,devel)'
-#NAME          READY     STATUS    RESTARTS   AGE       ENVIRONMENT   SOMETHING
-#kubia-label   1/1       Running   0          17m       prod          not-bad
-
-kubectl get po -l something=not-bad,environment=prod
-#NAME          READY     STATUS    RESTARTS   AGE       ENVIRONMENT   SOMETHING
-#kubia-label   1/1       Running   0          17m       prod          not-bad
-
-kubectl get po -l 'environment!=prod'
-#NAME           READY     STATUS    RESTARTS   AGE       ENVIRONMENT
-#kubia-manual   1/1       Running   0          30m       devel
-
-NODE=$(kubectl get node | awk 'FNR==2 {print $1}')
-kubectl label node $NODE gpu=true
+kubectl scale replicaset my-service --replicas=10
 
 cat <<EOF | kubectl create -f -
 apiVersion: v1
-kind: Pod
+kind: Service
 metadata:
-  name: lubia-gpu
+  name: my-service
 spec:
-  containers:
-  - image: luksa/kubia
-    name: kubia
-    ports:
-    - containerPort: 8080
-      protocol: TCP
-  nodeSelector:
-    gpu: "true"
+  type: ClusterIP
+  selector:
+    app: my-service
+  ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 80
 EOF
 
-#system namespace
-kubectl get po -n kube-system
+kubectl run --rm -it --image=alpine my-test
+apk -U add curl
+curl my-service
+curl my-service.default
+curl my-service.default.svc
+curl my-service.default.svc.cluster
+curl my-service.default.svc.cluster.local
 
-kubectl annotate pods kubia-label something.com/everything="this is quite nice and readable only from describe"
-kubectl get pods kubia-label -o yaml
-#...
-#metadata:
-#  annotations:
-#    kubernetes.io/limit-ranger: 'LimitRanger plugin set: cpu request for container kubia'
-#    something.com/everything: this is quite nice and readable only from describe
-#...
-
-kubectl create namespace wide-and-tried
+kubectl delete service my-service
 
 cat <<EOF | kubectl create -f -
 apiVersion: v1
-kind: Namespace
+kind: Service
 metadata:
-  name: tried-wide-and-ready-to-go
+  name: my-service
+spec:
+  type: LoadBalancer
+  selector:
+    app: my-service
+  ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 80
 EOF
 
-kubectl get ns
-#NAME                         STATUS    AGE       ENVIRONMENT   SOMETHING
-#default                      Active    1h                      
-#kube-public                  Active    1h                      
-#kube-system                  Active    1h                      
-#tried-wide-and-ready-to-go   Active    21s                     
-#wide-and-tried               Active    1m                      
+kubectl get svc
+curl $(kubectl get svc my-service | awk '{print $4}' | tail -1)
+HOST=`nslookup $(kubectl get svc my-service | awk '{print $4}' | tail -1) | grep name | cut -d'=' -f2 | sed 's/.$//' | sed 's/^.//'`
 
-kubectl delete po -l something
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: service2
+spec:
+  type: ClusterIP
+  selector:
+    app: my-service
+  ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 80
+EOF
 
-#switch default namespace
-kubens wide-and-tried
+cat <<EOF | kubectl create -f -
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: my-ingress
+spec:
+  rules:
+  - host $HOST
+    http:
+      paths:
+      - backend:
+        serviceName: my-service
+	servicePort: 80
+  - host: $HOST
+    http:
+      paths:
+      - backend:
+        serviceName: service2
+	servicePort: 80
+EOF
 
-kubectl delete ns wide-and-tried
-kubectl delete all --all
+cat <<EOF | kubectl create -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: my-deployment
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: my-deployment
+  template:
+    metadata:
+      labels:
+        app: my-deployment
+    spec:
+      containers:
+      - name: my-dep
+        image: luksa/kubia
+EOF
 
-kubectl config current-context
-#what is this??
-#kubectl config set-context ... --namespace
+kubectl scale deployments my-deployments --replicas=7
+kubectl edit deployment my-deployment
 
-gcloud container clusters delete kubia
+kubectl rollout undo deployment/my-deployment
+
+#https://cloud.google.com/kubernetes-engine/docs/concepts/persistent-volumes
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: herpvolume
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 30Gi
+EOF
+
+#sudo snap install helm
+#kubectl config view > /home/orpington/snap/helm/common/kube/config
+#sudo helm init
+#kubectl config view | sudo tee /root/snap/helm/common/kube/config
+#sudo helm init
+#
+#
