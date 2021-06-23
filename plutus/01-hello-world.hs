912df592035b568abeeb282497cfb44c3e3c41e5
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2021-06-23 11:35:31 +0100

    Add files via upload

---
+++ plutus/01-hello-world.hs
@@ -0,0 +1,15 @@
+import qualified Data.Text                 as T
+import           Language.Plutus.Contract  hiding (when)
+import           Language.PlutusTx.Prelude
+import           Playground.Contract
+
+-- | A 'Contract' that logs a message.
+hello :: Contract BlockchainActions T.Text ()
+hello = logInfo @String "herp merp"
+
+endpoints :: Contract BlockchainActions T.Text ()
+endpoints = hello
+
+mkSchemaDefinitions ''BlockchainActions
+
+$(mkKnownCurrencies [])
