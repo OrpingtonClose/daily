5ea82f2911d5bdc719b061a98832c4fb3e63990d
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2021-08-17 18:46:10 +0200

    Update yesod-basic.hs

--- haskell/yesod-basic.hs
+++ haskell/yesod-basic.hs
@@ -10,7 +10,29 @@ import Text.Hamlet (HtmlUrl, hamlet)
 
 data Piggies = Piggies
 
-instance Yesod Piggies
+--instance Yesod Piggies
+
+--myLayout :: Widget -> Handler Html
+myLayout widget = do
+    pc <- widgetToPageContent widget
+    withUrlRenderer
+        [hamlet|
+            $doctype 5
+            <html>
+                <head>
+                    <title>#{pageTitle pc}
+                    <meta charset=utf-8>
+                    <style>body { font-family: verdana }
+                    ^{pageHead pc}
+                <body>
+                    <article>
+                        <br>
+                        ^{pageBody pc}
+                        <img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/slideshows/is_my_cat_normal_slideshow/1800x1200_is_my_cat_normal_slideshow.jpg" alt="Girl in a jacket" width="100" height="100">                         
+        |]
+
+instance Yesod Piggies where
+    defaultLayout = myLayout
 
 someNumbers :: [String]
 someNumbers = ["hellp", "is it me", "your looking for"]
@@ -20,8 +42,90 @@ mkYesod "Piggies" [parseRoutes|
   /merp Merp GET
   /about AboutR GET
   /more More GET
+  /wid Wid GET
+  /trad Trade GET
 |]
+{-
+footer = [whamlet|
+<footer>
+    <a href=@{HomeR}>Homepage.
+|]
+-}
+--https://www.tradingview.com/widget/advanced-chart/
 
+tradingViewTest :: String -> WidgetFor Piggies ()
+tradingViewTest what = do
+    toWidget [whamlet|
+        <div style="display: inline-block; width: 200px;" class="tradingview-widget-container">
+            <div id="tradingview_81874">
+            <!-- <div class="tradingview-widget-copyright">
+                <a href="https://www.tradingview.com/symbols/XBTUSDT/?exchange=KRAKEN" rel="noopener" target="_blank">
+                    <span class="blue-text">XBTUSDT Chart
+                by TradingView -->
+            <script type="text/javascript" src="https://s3.tradingview.com/tv.js">
+            <script type="text/javascript">
+                new TradingView.widget(
+                {
+                    "width": 200,
+                    "height": 200,
+                    "symbol": "#{what}",
+                    "interval": "D",
+                    "timezone": "Etc/UTC",
+                    "theme": "light",
+                    "style": "1",
+                    "locale": "en",
+                    "toolbar_bg": "#f1f3f6",
+                    "enable_publishing": false,
+                    "hide_top_toolbar": true,
+                    "hide_legend": true,
+                    "save_image": false,
+                    "container_id": "tradingview_483d8"
+                }
+                );            
+    |]
+
+myWidget1 :: WidgetFor Piggies ()
+myWidget1 = do
+    toWidget [whamlet|
+            <h1>WIDGET HERE HEHE
+        |]
+    toWidget [lucius|h1 { color: green } |]
+
+
+footer :: WidgetFor Piggies ()
+footer = do
+    toWidget [lucius| footer { font-weight: bold; text-align: center} |]
+    toWidget [whamlet|
+            <footer>Return to #<a href=@{HomeR}>Homepage.</a>
+        |]
+    myWidget1
+
+--https://api.kraken.com/0/public/Assets
+tickers :: [String]
+tickers = ["1INCH","XBT","ADA","LINK"] ++ [
+  "1INCH",
+  "AAVE",
+  "ADA",
+  "ALGO",
+  "ANKR",
+  "ANT",
+  "ATOM",
+  "AXS",
+  "BADGER",
+  "BAL",
+  "BAND"]
+charts :: [String]
+charts = (\s -> "KRAKEN:" ++ s ++ "USD") <$> tickers
+
+getTrade :: HandlerFor Piggies Html
+getTrade = defaultLayout $ do
+    mapM_ tradingViewTest charts
+
+
+getWid :: HandlerFor Piggies Html
+getWid = defaultLayout footer
+
+getHomeR :: HandlerFor Piggies Html
 getHomeR = defaultLayout [whamlet|
   Welcome to the Pigsty!
   <br>
@@ -30,27 +134,17 @@ getHomeR = defaultLayout [whamlet|
   <a href=@{Merp}>About Us. Merp
   <br>
   <a href=@{More}>About Us. More
+  <br>
+  <a href=@{Wid}>About Us. Wid
+  <br>
+  <a href=@{Trade}>About Us. Wid
   ^{footer}
   |]
 
 --https://www.yesodweb.com/book/shakespearean-templates
 --footer :: HtmlUrl Piggies
-footer = [whamlet|
-<footer>
-    <a href=@{HomeR}>Homepage.
-|]
-{-
-myWidget1 = do
-    toWidget [hamlet|<h1>My Title|]
-    toWidget [lucius|h1 { color: green } |]
--}
-{-
-footer = do
-    toWidget [lucius| footer { font-weight: bold; text-align: center} |]
-    toWidget [whamlet|
-            <footer>Return to #<a href=@{HomeR}>Homepage.</a>
-        |]
--}
+
+
 getMerp = defaultLayout [whamlet|
   <html>
     <head>
