008dff2dda4f2f575fd78a2fef8824f05f72ca78
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2022-04-20 21:57:22 +0200

    Update game_of_life_03.py

--- python/game_of_life_03.py
+++ python/game_of_life_03.py
@@ -1,20 +1,21 @@
-#unfinished
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 
-width = 1000
-height = 1000
+width = 100
+height = 100
 
 fig = plt.figure()
-fig.set_size_inches(width / 100, height / 100)
+fig.set_size_inches(width /10, height/10)
 ax = fig.add_axes([0, 0, 1, 1], frameon=False, aspect=1)
 ax.set_xticks([])
 ax.set_yticks([])
 
 images = []
-board = np.random.default_rng().integers(0, 1, size=(length, width), endpoint=True) == 1
+
+board = np.random.default_rng().integers(0, 1, size=(height, width), endpoint=True) == 1
+#board = np.zeros((height, width))
+#board[width // 2] = 1
 
 zero_vertical = np.zeros((height, 1))
 zero_horizontal = np.zeros((1, width))
@@ -27,24 +28,32 @@ move_row_plus_1 = lambda b: nc0(b[1:,:], zero_horizontal)
 move_col_minus_1 = lambda b: nc1(zero_vertical, b[:,:-1])
 move_col_plus_1 = lambda b: nc1(b[:,1:], zero_vertical)
 
-coords = []
+images.append([plt.imshow(board, cmap='Greys', vmin=0, vmax=1, animated=True)])
 
-coords += [move_col_minus_1(move_row_minus_1(board))] # -1, -1
-coords += [move_col_minus_1(board)] # 0, -1
-coords += [move_row_plus_1(move_col_minus_1(board))] # +1, -1
-coords += [move_col_minus_1(board)] # -1, 0
-coords += [move_row_plus_1(board)] # +1, 0
-coords += [move_row_minus_1(move_col_plus_1(board))] # -1, +1
-coords += [move_col_plus_1(board)] # 0, +1
-coords += [move_col_plus_1(move_row_plus_1(board))] # +1, +1
+iterations = 250
+for i in range(iterations):
+    coords = []
 
+    coords += [move_col_minus_1(move_row_minus_1(board))] # -1, -1
+    coords += [move_col_minus_1(board)] # 0, -1
+    coords += [move_row_plus_1(move_col_minus_1(board))] # +1, -1
+    coords += [move_row_minus_1(board)] # -1, 0
+    coords += [move_row_plus_1(board)] # +1, 0
+    coords += [move_row_minus_1(move_col_plus_1(board))] # -1, +1
+    coords += [move_col_plus_1(board)] # 0, +1
+    coords += [move_col_plus_1(move_row_plus_1(board))] # +1, +1
 
+    neighbour_sums = board.copy() + sum(coords)
+    new_board = board.copy()
+    board_make_not_alive = np.isin(neighbour_sums, [0, 1, 4, 5, 6, 7, 8]) & (board == 1)
+    board_make_alive = np.isin(neighbour_sums, [3]) & (board == 0)
 
-#np.random.default_rng().integers(0, 1, size=(length, width), endpoint=True) == 1
-#plt.imshow(np.random.default_rng().integers(0, 1, size=(length, width), endpoint=True) == 1, cmap='Greys', vmin=0, vmax=1, animated=True)
+    new_board[board_make_not_alive] = 0
+    new_board[board_make_alive] = 1
 
-for _ in range(100):
-    images.append([])
+    images.append([plt.imshow(new_board.astype("int64"), cmap='Greys', vmin=0, vmax=1, animated=True)])
+    board = new_board
+    print(iterations, " out of ", i)
 
 animation = anim.ArtistAnimation(fig, images)
-animation.save("random.gif", writer='imagemagick', fps=10)
+animation.save("random.gif", writer='imagemagick', fps=25)
