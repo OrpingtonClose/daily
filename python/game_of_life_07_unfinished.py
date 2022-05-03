422bd9022027add44725df2a0b63d97fba3d927a
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2022-05-03 07:39:12 +0200

    Update game_of_life_07_unfinished.py

--- python/game_of_life_07_unfinished.py
+++ python/game_of_life_07_unfinished.py
@@ -162,3 +162,65 @@ for _ in range(100):
     # plt.tight_layout()
     # plt.show()
 
+# ==================================================
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn.apionly as sns
+import matplotlib.animation
+
+# Create Graph
+# np.random.seed(2)
+# G = nx.cubical_graph()
+# G = nx.relabel_nodes(G, {0:"O", 1:"X", 2:"XZ", 3:"Z", 4:"Y", 5:"YZ", 6: "XYZ", 7:"XY"})
+G = nx.grid_2d_graph(25, 50)
+pos = nx.spring_layout(G)
+
+# Sequence of letters
+#sequence_of_letters = "".join(['X', 'Y', 'Z', 'Y', 'Y', 'Z'])
+#idx_colors = sns.cubehelix_palette(5, start=.5, rot=-.75)[::-1]
+#idx_weights = [3,2,1]
+
+# Build plot
+fig, ax = plt.subplots(figsize=(6,4))
+
+
+def update(num):
+    # G = nx.grid_2d_graph(5, 5)  # 5x5 grid
+    # This example needs Graphviz and PyGraphviz
+    #nx.nx_agraph.write_dot(G, "grid.dot")
+    # Having created the dot file, graphviz can be invoked via the command line
+    # to generate an image on disk, e.g.
+    # print("Now run: neato -Tps grid.dot >grid.ps")
+
+    # Alternatively, the and image can be created directly via AGraph.draw
+    # A = nx.nx_agraph.to_agraph(G)
+    # A.draw("5x5.png", prog="neato")
+
+    ax.clear()
+    #i = num // 3
+    #j = num % 3 + 1
+    #triad = sequence_of_letters[i:i+3]
+    #path = ["O"] + ["".join(sorted(set(triad[:k + 1]))) for k in range(j)]
+
+    # # Background nodes
+    # nx.draw_networkx_edges(G, pos=pos, ax=ax, edge_color="gray")
+    # null_nodes = nx.draw_networkx_nodes(G, pos=pos, nodelist=set(G.nodes()) - set(path), node_color="white",  ax=ax)
+    # null_nodes.set_edgecolor("black")
+
+    # # Query nodes
+    # query_nodes = nx.draw_networkx_nodes(G, pos=pos, nodelist=path, node_color=idx_colors[:len(path)], ax=ax)
+    # query_nodes.set_edgecolor("white")
+    # nx.draw_networkx_labels(G, pos=pos, labels=dict(zip(path,path)),  font_color="white", ax=ax)
+    #edgelist = [path[k:k+2] for k in range(len(path) - 1)]
+    #nx.draw_networkx_edges(G, pos=pos, edgelist=edgelist, width=idx_weights[:len(path)], ax=ax)
+    nx.draw_networkx_nodes(G, pos=pos, ax=ax)
+
+    # # Scale plot ax
+    # ax.set_title("Frame %d:    "%(num+1) +  " - ".join(path), fontweight="bold")
+    # ax.set_xticks([])
+    # ax.set_yticks([])
+
+
+ani = matplotlib.animation.FuncAnimation(fig, update, frames=6, interval=1000, repeat=True)
+plt.show()
