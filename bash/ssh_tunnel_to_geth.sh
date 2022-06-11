1c5e07b0655b63af93322b62033e0ef81ca6d9fd
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2022-06-11 21:35:12 +0200

    Update ssh_tunnel_to_geth.sh

--- bash/ssh_tunnel_to_geth.sh
+++ bash/ssh_tunnel_to_geth.sh
@@ -1,7 +1,7 @@
-EXTERNAL_IP=40.127.106.43
+EXTERNAL_IP=40.113.229.209
 EXTERNAL_PORT=8545
 INTERNAL_PORT=8545
 EXTERNAL_USER=orpington
 
 ssh -f -N -L $INTERNAL_PORT:localhost:$EXTERNAL_PORT $EXTERNAL_USER@$EXTERNAL_IP
-curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' http://$EXTERNAL_IP:$EXTERNAL_PORT
+curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' http://localhost:$INTERNAL_PORT
