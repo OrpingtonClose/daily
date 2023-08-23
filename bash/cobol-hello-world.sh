5974f5b3e80b5d3b2a60f0ee3202cf83d4f66f81
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2023-08-23 13:42:10 +0200

    Update cobol-hello-world.sh

--- bash/cobol-hello-world.sh
+++ bash/cobol-hello-world.sh
@@ -1,11 +1,11 @@
 sudo apt-get install open-cobol -y
 cat << EOF > helloworld.cbl
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. HELLO-WORLD.
-      * simple hello world program
-       PROCEDURE DIVISION.
-           DISPLAY 'Hello world!'.
-           STOP RUN.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. HELLO-WORLD.
+* simple hello world program
+PROCEDURE DIVISION.
+       DISPLAY 'Hello world!'.
+       STOP RUN.
 EOF
 cobc -free -x -o helloworld helloworld.cbl
 ./helloworld
