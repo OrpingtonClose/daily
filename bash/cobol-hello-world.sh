231a10ac08fc5dd886bad9eafdb69956a4527e9b
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2023-08-23 13:39:55 +0200

    Create cobol-hello-world.sh

---
+++ bash/cobol-hello-world.sh
@@ -0,0 +1,11 @@
+sudo apt-get install open-cobol -y
+cat << EOF > helloworld.cbl
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+      * simple hello world program
+       PROCEDURE DIVISION.
+           DISPLAY 'Hello world!'.
+           STOP RUN.
+EOF
+cobc -free -x -o helloworld helloworld.cbl
+./helloworld
