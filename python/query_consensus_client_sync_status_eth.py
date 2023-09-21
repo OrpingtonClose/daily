1575aca97bc1cdf2566f4b249a5ff3ff99aa43d8
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2023-09-22 00:17:16 +0200

    Update query_consensus_client_sync_status_eth.py

--- python/query_consensus_client_sync_status_eth.py
+++ python/query_consensus_client_sync_status_eth.py
@@ -1,18 +1,8 @@
-import click
+import fire
 import requests
 
-@click.command("hello")
-@click.version_option("0.1.0", prog_name="hello")
-def hello():
-  click.echo("Hello, World!")
-
-@click.command("syn")
-def syn():
-  click.echo(str(int(r.json()['data']['head_slot'])  +int( r.json()['data']['sync_distance'])))
-
-
-def go():
-  r = requests.get('http://127.0.0.1:3500/eth/v1/node/syncing')
+def consensus():
+  r = requests.get('http://127.0.0.1:3500/edth/v1/node/syncing')
   d = r.json()['data']
   head = int(d['head_slot'])
   distance = int(d['sync_distance'])
@@ -23,5 +13,49 @@ def go():
   print("total blocks: {:_>30}".format(al_blocks))
   print("ratio: {:_>30.2f}".format(ratio))
 
+def execution(t):
+    import requests
+    import re
+    import time
+    print(t)
+    req = lambda: requests.get('http://127.0.0.1:8545', headers={"Content-type": "application/json"}, json={"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1})
+    rs = []
+    res = dict()
+    for _ in range(t):
+        time.sleep(5)
+        print("fetching")
+        rs.append(req())
+    for r in rs:
+        for key, value in {k: int(v, 16) for k, v in r.json()["result"].items()}.items():
+            k = res.get(key, [])
+            k.append(value)
+            res[key] = k
+    while True:
+        for key, val in res.items():
+            print_string = ""
+            key_formatted = " ".join([m.group(0).lower() for m in re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', key)])
+            print_string = f"{key_formatted:.<25} "
+            i = 0
+        for key, val in res.items():
+            print_string = ""
+            key_formatted = " ".join([m.group(0).lower() for m in re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', key)])
+            print_string = f"{key_formatted:.<25} "
+            i = 0
+            for v in val:
+                if i == 0:
+                  val_formatted = format(v, ',').replace(',', ' ').replace('.', ',')
+                else:
+                  val_formatted = format(v - val[i - 1], ',').replace(',', ' ').replace('.', ',')
+                i += 1
+                print_string += f"{val_formatted:.>15}:"
+            print(print_string)
+        print("=" * len(print_string))
+        time.sleep(5)
+        for key, value in {k: int(v, 16) for k, v in req().json()["result"].items()}.items():
+            k = res.get(key, [])
+            k.append(value)
+            del k[0]
+            res[key] = k
+
 if __name__ == "__main__":
-  go()
+  fire.Fire()
